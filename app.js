a4ddf91fd13b4b34f6710e54ed6cb2e72a74e777
Luis Felipe <luis.felipe.vieira@outlook.com>
2023-11-02 20:55:18 -0300

    atualização 02/11/2023

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 const express = require("express");
+const checkListsRouter = require("./src/routes/checklist");
+
+const app = express();
+app.use(express.json()); // verifica se há um json
+
+app.use("/checklists", checkListsRouter);
+
+app.listen(3000, () => {
+  console.log("Servidor foi iniciado");
+});
