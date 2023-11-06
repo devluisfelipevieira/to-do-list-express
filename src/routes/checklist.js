1e0591e4af11d5657407799caa2456280e083b86
Luis Felipe <luis.felipe.vieira@outlook.com>
2023-11-06 19:34:36 -0300

    atualização do curso

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -2,19 +2,54 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  console.log("Hello World");
-  res.send();
+const Checklist = require("../models/checklist");
+
+router.get("/", async (req, res) => {
+  try {
+    let checklist = await Checklist.find({}); // lista as checklist
+    res.status(200).json(checklist);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.post("/", (req, res) => {
-  console.log(req.body);
+router.post("/", async (req, res) => {
+  let { name } = req.body; // desestruturação do name no body
+
+  try {
+    let checklist = await Checklist.create({ name }); // cria uma checklist
+    res.status(200).json(checklist);
+  } catch (error) {
+    res.status(422).json(error);
+  }
+
+  console.log(name);
   res.status(200).json(req.body);
 });
 
-router.get("/:id", (req, res) => {
-  console.log(req.params.id);
-  res.send(`ID: ${req.params.id}`);
+router.get("/:id", async (req, res) => {
+  try {
+    let checklist = await Checklist.findById(req.params.id); //procurando pelo ID
+    res.status(200).json(checklist);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  let { name } = req.body;
+  let id = req.params.id;
+
+  try {
+    let checklist = await Checklist.findByIdAndUpdate(
+      id,
+      { name },
+      { new: true }
+    );
+    res.status(200).json(checklist);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
